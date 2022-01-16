86735e85d827be25f9ed67b3afc1f928e130ec39
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2022-01-17 01:00:33 +0300

    Create MainActivity

---
+++ app/src/main/java/com/example/dictionary/frameworks/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dictionary.frameworks.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dictionary.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
